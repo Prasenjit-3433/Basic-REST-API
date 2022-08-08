2e47b4029d5b0f0a889e9291f0871fb728eadc69
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2022-08-08 09:12:10 +0530

    created random quote generation feature

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const db = require('./data/database');
 const quoteRoutes = require('./routes/quotes.routes');
 
 const app = express();
@@ -7,4 +8,17 @@ const app = express();
 
 app.use('/quote', quoteRoutes);
 
-app.listen(3000);
+app.use(function(error, req, res, next) {
+    res.status(500).json({
+        message: 'Something went wrong!'
+    });
+});
+
+db.initDb()
+.then(function() {
+    app.listen(3000);
+})
+.catch(function(error) {
+    console.log('Connecting to the database failed!');
+    console.log(error);
+});
--- controllers/quotes.controller.js
+++ controllers/quotes.controller.js
@@ -1,6 +1,15 @@
-function getRandomQuote(req, res, next) {
+const Quote = require('../models/quote.model');
+
+async function getRandomQuote(req, res, next) {
+    let randomQuote;
+    
+    try {
+        randomQuote = await Quote.getRandomQuote();
+    } catch (error) {
+        return next(error);
+    }
     res.json({
-        quote: 'A quote...'
+        quote: randomQuote
     });
 }
 
